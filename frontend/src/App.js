da28f6cdcdf4abb96b7c9aa6efc002ed97f19598
dominicruot <dominicruot@gmail.com>
2022-05-28 12:42:10 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import './bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h4 className="App-logo" alt="logo">DAYARI APP!</h4>
+
+        <p>
+          Hello world.
+          This app is underconstruction!
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
